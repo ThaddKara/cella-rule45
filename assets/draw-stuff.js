eef7c91ee38736f213d11598d7f2dd42f3ad12f1
Zak <34078361+BoSkeet@users.noreply.github.com>
2019-02-15 22:26:04 -0800

    Updates

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -32,7 +32,7 @@ function draw_grid( rctx, rminor, rmajor, rstroke, rfill  )
         rctx.lineTo( ix, height );
         rctx.lineWidth = ( ix % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
+        //if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
     }
     for ( var iy = 0; iy < height; iy += rminor )
     {
@@ -41,7 +41,14 @@ function draw_grid( rctx, rminor, rmajor, rstroke, rfill  )
         rctx.lineTo( width, iy );
         rctx.lineWidth = ( iy % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
+        //if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
     }
     rctx.restore( );
+}
+
+function fill_first( ctx )
+{
+    ctx.save( );
+    draw_rect()
+    
 }
